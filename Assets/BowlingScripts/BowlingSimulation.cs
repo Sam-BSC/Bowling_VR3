b221bef5d463ad4eb70aaf45a6c49fa24d578008
SamF <saf@bundoorasc.vic.edu.au>
2024-06-06 15:33:36 +1000

    Original Code
    Change Bowling Ball and Reset

---
+++ Assets/BowlingScripts/BowlingSimulation.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingSimulation : MonoBehaviour
+{
+    // this is a transform in the scene for usshi'fang to specify the initial velocity of the bowling ball.
+    [Tooltip("Forward direction to be used as test initial direction.")]
+    public Transform testVelocityDirection;
+    
+    [Tooltip("How fast the test ball will go?")]
+    public float testVelocityMagnitude;
+
+    public Rigidbody testBowlingBall;
+    public Vector3 bowlingBallStartPosition;
+    public PinsManager pinsManager;
+
+    public void Start()
+    {
+        //StartCoroutine(PendTestLaunch());
+        bowlingBallStartPosition = testBowlingBall.position;
+    }
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(PendTestLaunch());
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+
+            pinsManager.ResetAllPins();
+
+            testBowlingBall.isKinematic = true;
+            testBowlingBall.position = bowlingBallStartPosition;
+        }
+    }
+    // wait for 2 seconds and launch the ball
+    IEnumerator PendTestLaunch()
+    {
+        //yield return new WaitForSeconds(2f);
+
+        testBowlingBall.isKinematic = false;
+        //testBowlingBall.useGravity = true;
+        yield return null;
+
+        Launch(testVelocityDirection.forward * testVelocityMagnitude);
+    }
+    private void Launch(Vector3 initialVelcity)
+    {
+        Debug.Log("Launch: Values");
+
+        testBowlingBall.AddForce(initialVelcity, ForceMode.VelocityChange);
+    }
+}
